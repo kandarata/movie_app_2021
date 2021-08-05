b7e6897901eaa780bfe4b97884686816d6935c0c
rino <kandarata@gmail.com>
2021-08-05 17:16:26 +0900

    dafd

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
 import React from "react";
+import ProTypes from "prop-types";
+
+const foodILike = [
+	{
+		id:1,
+		name : "test",
+		image: "test.jpg"
+	}
+];
+
+function Food({name}, {picture}) {
+	return (
+		<div>
+			<h2>I like {name}</h2>
+			<img src={picture} alt={name}/>
+		</div>
+	);
+}
+
+Food.proTypes = {
+
+}
 
 function App() {
-  return <div>Movie App 2021</div>;
+  return (
+	<div>
+		{foddILike.map(dish => (
+			<Food key={dish.id} name={dish.name} picture={dish.image} />
+		))}
+	</div>
+  );
 }
 
 export default App;
