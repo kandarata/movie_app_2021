05ca1d52bf342bc9af000b6a633ed4272f9a59fb
kandarata <kandarata@gmail.com>
2021-08-09 11:06:01 +0900

    1234

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React from "react";
 
 class App extends React.Component {
+  
   state = {
     isLoading: true,
     movies: []
-  };
+  }
   
   componentDidMount() {
     setTimeout(() => {
       this.setState({ isLoading: false, book: true });
     }, 6000);
-  };
+  }
   
   render() {
     const { isLoading } = this.state;
     return (
       <div>{ isLoading ? "Loading..." : "We are ready" }</div>
     );
-  };
+  }
+  
 }
