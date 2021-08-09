90b01c19093290962d5d3a3c83200a16cf4761a8
kandarata <kandarata@gmail.com>
2021-08-09 10:52:03 +0900

    ㄴㅇㄹ

--- src/App.js
+++ src/App.js
@@ -1,45 +1,19 @@
 import React from "react";
 
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-        console.log("hello");
-    }
-
-    state = {
-        count : 0
-    };
-
-    add = () => {
-        this.setState({count: this.state.count + 1});
-    }
-
-    minus = () => {
-        this.setState({count: this.state.count - 1});
-    }
-
-    componentDidMount() {
-        console.log("component rendered");
-    }
-
-    componentDidUpdate() {
-        console.log("component update");
-    }
-
-    componentWillUnmount() {
-        console.log("component Unmounted");
-    }
-
-    render() {
-        console.log("I'm rendering");
-        return (
-            <div>
-                <h1>this is number : {this.state.count}</h1>
-                <button onClick={this.add}>add</button>
-                <button onClick={this.minus}>minus</button>
-            </div>
-	);
-    }
+class App() extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
+  
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false, book: true });
+    }, 6000);
+  }
+  
+  render() {
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}<div>;
+  }
 }
-
-export default App;
